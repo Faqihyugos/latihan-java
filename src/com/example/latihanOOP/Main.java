0654a3c480075edcf0aee604244e5f9f3de72259
faqihyugos <faqihzero795@gmail.com>
2022-05-14 12:57:49 +0700

    input distance
    input distance with scanner

--- src/com/example/latihanOOP/Main.java
+++ src/com/example/latihanOOP/Main.java
@@ -1,11 +1,16 @@
 package com.example.latihanOOP;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Bicycle bicycle = new Bicycle("Bianchi","Hijau");
         System.out.println(" [Info Sepeda] ");
         bicycle.printData();
         System.out.println("-----------------");
-        bicycle.run(10);
+        System.out.print("Masukkan jarak yang akan ditempuh: ");
+        int bicycleDistance = scanner.nextInt();
+        bicycle.run(bicycleDistance);
     }
 }
